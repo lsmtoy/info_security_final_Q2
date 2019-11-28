190a6bd5ca8bc0b0472f39b2cda5e7fbda162d76
이승민 <lsmtoy@naver.com>
2019-11-28 14:23:45 +0900

    final

--- main.py
+++ main.py
@@ -11,55 +11,47 @@ def mod(base, exp, divider):
             cnt = cnt + 1
         else:
             temp = (temp ** (2 ** cnt)) % divider
-            # result = (result * temp) % divider
+            result = (result * temp) % divider
             # print('i is: ', i, '\t', c,'^',2**i,'mod',n,'is:\t',temp_m,'\tm is:',m)
             cnt = 1
         exp = exp >> 1
-    print('result is: ', result)
+    #print('result is: ', result)
     return result
 
 
 def find_disc_log(base, divider, result):
     for i in range(1,divider):
-        if mod(base, i, divider) == result:
-            print(base,'^',i,'mod',divider,'is: ',result)
+        #if mod(base, i, divider) == result:
+        if base ** i % divider == result:
+            #print(base,'^',i,'mod',divider,'is: ',result)
             return i
     print('no answer')
 
 
 if __name__ == '__main__':
-    '''
+
     q = 2934201397
     a = 37
     Ya = 2174919958     # Ya = a ^ Xa   mod
     Xa = 83
-    #C1 = 2909170161; C2 = 2565161545
-    C1 = 2909170161; C2 = 1004005362       # M = 189465461
+    C1 = 2909170161; C2 = 2565161545
+    #C1 = 2909170161; C2 = 1004005362       # M = 189465461
     #C1 = 2909170161; C2 = 2081016632       # M = 848963461
-    '''
 
+    print('C1 is:',C1,'\tC2 is:',C2)
+
+    '''
     q = 19
     a = 10
     Ya = 3  # Ya = a ^ Xa   mod
     Xa = 1  # Xa = 5
     C1 = 11 ; C2 = 5
+    '''
 
     Xa = find_disc_log(a, q, Ya)
+
     K = mod(C1, Xa, q)
     inverse_K = 1
-
-    '''
-    Xa의 값을 구하는 코드
-    temp = 1
-    for i in range (1, q-1):
-        print("i is: ", i)
-        temp = temp*a
-        if(temp % q == Ya):
-            Xa = i
-            break
-    '''
-
-    K = mod(a,Xa,q)
     print('K:',K)
 
     '''
@@ -88,7 +80,6 @@ if __name__ == '__main__':
             b1 = t1; b2 = t2; b3 = t3;
 
     print('inverse_K is: ', inverse_K)
-    #print( K*(inverse_K + q) % q)
     print('M is: ', C2*inverse_K % q)
 
 
